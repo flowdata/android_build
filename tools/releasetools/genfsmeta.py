e960bb0a2041c32dd8016ffd1242a97f98f5692e
EK Thomson <ekthomson@gmail.com>
2016-02-26 20:05:01 -0700

    Fix bug

--- tools/releasetools/genfsmeta.py
+++ tools/releasetools/genfsmeta.py
@@ -12,7 +12,7 @@ def genLine(base, path, pointer):
     print("%s %s %s %s %s" % (subname, stats.st_uid, stats.st_gid, realmode, "selabel=u:object_r:unlabeled:s0 capabilities=0x0"))
 
 def genMetaFile(base, directory):
-    genLine(base, os.path.join(base, directory))
+    genLine(base, os.path.join(base, directory), "")
     for root, dirs, files in os.walk(os.path.join(base, directory)):
         for directory in dirs:
             genLine(base, root, directory)
